c4d3527ca24c3d91cc7b8a9f6410802f828fbe6e
Syed Amaan <75304633+Ak-407@users.noreply.github.com>
2025-02-16 16:15:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -770,9 +770,9 @@ def chat():
 
 
 
-
-
-
+import os
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Default to port 5000 if not set
+    app.run(host='0.0.0.0', port=port, debug=True)
+
